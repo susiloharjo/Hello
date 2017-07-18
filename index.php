79b98df574849164d8ab7eaef84704ef5de81908
susiloharjo <susiloharjo@gmail.com>
2017-07-18 15:19:50 +0700

    my web

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Susiloharjo.web.id</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" rel="stylesheet">
+  <!-- Optional theme -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script><style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font: 400 15px/1.8 "Lato", sans-serif;
+  color: #777;
+}
+
+.bgimg-1, .bgimg-2, .bgimg-3,.bgimg-3a {
+  position: relative;
+  opacity: 0.65;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+.bgimg-1 {
+  background-image: url("images/2.jpg");
+  min-height: 100%;
+}
+
+.bgimg-2 {
+  background-image: url("images/4.jpg");
+  min-height: 400px;
+}
+
+.bgimg-3 {
+  background-image: url("images/5.jpg");
+  min-height: 400px;
+}
+
+.bgimg-3a {
+  background-image: url("images/1.jpg");
+  min-height: 120px;
+}
+
+.caption {
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 100%;
+  text-align: center;
+  color: #000;
+}
+
+.caption span.border {
+  background-color: #111;
+  color: #fff;
+  padding: 18px;
+  font-size: 25px;
+  letter-spacing: 10px;
+}
+
+h3 {
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  font: 20px "Lato", sans-serif;
+  color: #111;
+}
+
+/* Turn off parallax scrolling for tablets and phones */
+@media only screen and (max-device-width: 1024px) {
+    .bgimg-1, .bgimg-2, .bgimg-3 {
+        background-attachment: scroll;
+    }
+}
+</style>
+</head>
+<body>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border">welcome @ susiloharjo.web.id</span>
+    <br>
+    <br>
+    <span  class="border"><a style="color:#fefefe;  text-decoration: none;" href="#about">About Me</a></span>
+  </div>
+</div>
+
+<div id="about" style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3 style="text-align:center;">About Me</h3>
+  <p align="center"><strong>#Husband, #Father, #Fotografi, #Robot, #WebDeveloper, #Thinker, #Linux, #Ubuntu, #Blogger</strong>  </p>
+  <p> Aku diberi nama Eko Susilo Harjo oleh kedua Orang Tua ku, Anak pertama yang dilahirkan di kota kecil bernama Sangasanga, disinilah aku dibesarkan tepatnya adalah sebuah kota perjuangan di Kabupaten Kutai Kartanegara Kalimantan Timur</p>
+  <p>Kuliah membawaku ke Yogyakarta, Jurusan Teknik Elektro Fakultas Teknologi Industri Universitas Islam Indonesia adalah tempat ku menghabiskan hari-hariku di yogya. Ketertarikanku pada dunia elektronika mungkin menurun oleh bapak yang seorang electrical technician jadinya yah anaknya juga ingin seperti bapaknya, kata orang “like father like son” 🙂 .</p>
+  <p>Selain dunia elektro aku juga tertarik pada dunia IT ini dimulai ketika aku mulai mengenal website lalu kemudian membuatnya, selain website ada lagi sebuah dunia baru buatku yang masih informatika banget mungkin, jadi setelah belajar jadi webmaster ketertarikan selanjutnya adalah belajar menjadi network administrator dan ikut cisco network course di UII, selain itu pula ada seorang teman yang linuxaholic mengenalkanku pada linux dan akhirnya sampai sekarang tidak bisa lepas pake linux  “linux is for future”.</p>
+  <p>Dan pada akhirnya gabungan dari semua itu yang hasilnya membuatku lulus dikampus dengan Tugas Akhir” Deteksi Gerak Menggunakan Webcam dengan Tampilan Pada Halaman Web” dengan menggunakan Linux sebagai webserver dan videoserver. </p>
+  <p>Aktifitas saya sehari hari adalah sebagai seorang Safety Supervisor di perusahaan yang bergerak di bidang radio dan telekomunikasi, selain pekerjaan tersebut hobby saya yang lain adalah
+  coding web, design 3d dan ngutak ngatik mesin 3d Printer, dan juga saya lagi suka main arduino untuk IOT, Robotika dan Automasi, saya juga suka berkebun (hydroponic).</p>
+  <p>Bagi rekan rekan sekalian yang ingin melihat keseharian saya silahkan lihat lihat di social media dibawah</p>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LATEST PROJECT</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+    <h3 style="color:#fefefe;text-align:center;">latest project</h3>
+    <br>
+    <div class="container">
+    <div class="row">
+
+      <div class="col-lg-12">
+          <div class="col-md-4">
+            <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/e2_G8DGa_Ds?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-md-4">
+            <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/6vhvL3CmxyA?list=PLSqMj83kcvHNKuczKX9JmkuOSaOEdaAl-?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-md-4">
+          <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/fCOqvFKbS2o?list=PLSqMj83kcvHOJI9eGSV6p1lCI33JJi3SW?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-lg-12">
+            <hr>
+          </div>
+          <div class="col-md-4">
+          <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/A2gihnYunYk?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-md-4">
+          <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/z0jAuknaNwU?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-md-4">
+            <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/SSXNhupm3pE?list=PLSqMj83kcvHNIrTpStLN0awP5y0UX4srt?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-lg-12">
+            <hr>
+          </div>
+          <div class="col-md-4">
+          <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/OQALOAhFToA?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+          </div>
+          <div class="col-md-4">
+        <div style="position:relative;height:0;padding-bottom:75.0%"><iframe src="https://www.youtube.com/embed/kmoBt6KaP5o?ecver=2" width="480" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+        </div>
+          <div class="col-md-4">
+        <div style="position:relative;height:0;padding-bottom:75.0%"><iframe src="https://www.youtube.com/embed/73-UpVSEB4s?ecver=2" width="480" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+        </div>
+      </div>
+
+        </div>
+    </div>
+  </div>
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">CONTACT</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+    <h3 style="color:#fefefe;text-align:center;">Contact</h3>
+    <div align="center">
+
+    <a style="color:#fefefe; font-size:28px;" target="blank" href="http://facebook.com/susiloharjo"><i class="fa fa-facebook-official"></i> </a>
+     <a style="color:#fefefe; font-size:28px;" target="blank" href="http://instagram.com/susiloharjo"><i class="fa fa-instagram"></i> </a>
+     <a style="color:#fefefe; font-size:28px;" target="blank" href="http://kakeko.wordpress.com"><i class="fa fa-wordpress"></i> </a>
+     <a style="color:#fefefe; font-size:28px;" target="blank" href="https://www.youtube.com/user/susiloharjo"><i class="fa  fa-youtube-play"></i> </a>
+     <a style="color:#fefefe; font-size:28px;" target="blank" href="mailto:susiloharjo@gmail.com?subject=susiloharjo.web.id"><i class="fa  fa-envelope-o"></i></a>
+     <a style="color:#fefefe; font-size:28px;" target="blank" href="https://www.flickr.com/photos/susiloharjo"><i class="fa  fa-flickr"></i> </a>
+
+     </div>
+  </div>
+</div>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</body>
+
+</html>
